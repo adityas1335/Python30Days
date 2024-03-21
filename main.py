f0bf3571c988daf31301df99b5d89caa327e33af
Adiya Singh <108605877+adityas1335@users.noreply.github.com>
2024-03-21 11:25:48 +0000

    A Youtube Channel Name Generator

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("Hello World")
+# A Basic youtube channel name generator
+
+name = input("What is your First name? ")
+print("Hello " + name)
+topic = input("What is your channel topic category? such as: Gaming, Music, etc. ")
+print("Your channel name could be \"" + topic + " with " + name+"\"")
+print(f"Your channel name could be \"{name}\'s {topic}\"")
