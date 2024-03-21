f09a5553faf312742c203a362a538c5863abf7d7
Adiya Singh <108605877+adityas1335@users.noreply.github.com>
2024-03-21 19:48:05 +0000

    integrated other files to main.py file

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 # HOME
 
+user_choice = input("Which module do you want to work with? Enter 1 for BMIcalculor, 2 for YTNameGenerator: ")
+
+if user_choice == '1':
+    import BMIcalculor
+elif user_choice == '2':
+    import YTNameGenerator
+else:
+    print("Invalid choice. Please enter 1 or 2.")
+
