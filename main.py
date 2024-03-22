3344a04447210736fae048e8553e1d0c86295415
Adiya Singh <108605877+adityas1335@users.noreply.github.com>
2024-03-22 14:17:26 +0000

    modified: added 3 option as R_P_C_Game

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 # HOME
 
-user_choice = input("Which module do you want to work with? Enter 1 for BMIcalculor, 2 for YTNameGenerator: ")
+user_choice = input(" 1 for BMIcalculor\n 2 for YTNameGenerator \n 3 for R_P_C_Game \nWhich module do you want to work with?\n Enter: ")
 
 if user_choice == '1':
     import BMIcalculor
 elif user_choice == '2':
     import YTNameGenerator
+elif user_choice == '3':
+     import R_P_C_Game
 else:
     print("Invalid choice. Please enter 1 or 2.")
 
+    # run the program again user want
+user_choice = input("Do you want to run again? Enter 'Y' to continue: ")
+if user_choice.lower() == 'y':
+  import main
+else:
+  print("Thank you for using. Goodbye!")
