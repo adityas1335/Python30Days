d65cb7aa659fa75d24abbf395fb8372177bd017d
Adiya Singh <108605877+adityas1335@users.noreply.github.com>
2024-03-26 19:17:58 +0000

    Added Finding Treasure game in main page

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 # HOME
 
-user_choice = input(" 1 for BMIcalculor\n 2 for YTNameGenerator \n 3 for R_P_C_Game \nWhich module do you want to work with?\n Enter: ")
+user_choice = input(
+    " 1 for BMIcalculor\n 2 for YTNameGenerator \n 3 for R_P_C_Game \n 4 Finding Treasure \nWhich module do you want to work with?\n Enter: "
+)
 
 if user_choice == '1':
-    import BMIcalculor
+  import BMIcalculor
 elif user_choice == '2':
-    import YTNameGenerator
+  import YTNameGenerator
 elif user_choice == '3':
-    import R_P_C_Game
+  import R_P_C_Game
+elif user_choice == '4':
+  import FindTreasure
 else:
-    print("Invalid choice. Please enter 1 or 2 or 3.")
+  print("Invalid choice. Please enter 1 or 2 or 3 or 4.")
 
-    # run the program again user want
+  # run the program again user want
 user_choice = input("Do you want to run again? Enter 'Y' to continue: ")
 if user_choice.lower() == 'y':
   import main
